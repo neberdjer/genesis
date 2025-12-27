a1155589d53450115501e461e47389160cb9d2f9
creations <creations@creations.works>
2025-12-27 11:03:22 -0500

    new migration instead

--- migrations/006_add_welcome_settings.sql
+++ migrations/006_add_welcome_settings.sql
@@ -1,4 +1,3 @@
 ALTER TABLE server_settings ADD COLUMN IF NOT EXISTS welcome_enabled BOOLEAN DEFAULT FALSE;
 ALTER TABLE server_settings ADD COLUMN IF NOT EXISTS welcome_channel_id TEXT;
 ALTER TABLE server_settings ADD COLUMN IF NOT EXISTS welcome_message TEXT DEFAULT 'Welcome to {server_name}, {user}';
-ALTER TABLE server_settings ADD COLUMN IF NOT EXISTS welcome_role_id TEXT;
---
+++ migrations/007_add_welcome_role.sql
@@ -0,0 +1,1 @@
+ALTER TABLE server_settings ADD COLUMN IF NOT EXISTS welcome_role_id TEXT;
